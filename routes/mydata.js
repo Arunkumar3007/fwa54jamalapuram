9f91c2100f0c7cdebf1961539c27c9e118b9f672
Arunkumar3007 <89620964+Arunkumar3007@users.noreply.github.com>
2021-10-27 17:33:47 -0500

    added Users

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ARUN KUMAR' });
+  res.render('mydata', { title: 'ARUN KUMAR' });
 });
 
 module.exports = router;
